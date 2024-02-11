d629af5fffd5a4c2d231ab6c21a01a1eb9584e16
sviatoslav <b.svjatoslav@gmail.com>
2024-02-11 22:29:58 +0200

    Fixed

---
+++ mmdet/configs/rtmdet/anton_config.py
@@ -0,0 +1,105 @@
+
+_base_ = r".\rtmdet_tiny_8xb32-300e_coco.py"
+
+data_root = r"..\..\..\converted_rsna"
+
+train_batch_size_per_gpu = 32
+train_num_workers = 1
+
+max_epochs = 20
+stage2_num_epochs = 5
+base_lr = 0.00008
+
+
+metainfo = {
+    'classes': ('pneumonia'),
+    'palette': [
+        (220, 20, 60),
+    ]
+}
+
+train_dataloader = dict(
+    batch_size=train_batch_size_per_gpu,
+    num_workers=train_num_workers,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        data_prefix=dict(img='train_data_converted'),
+        ann_file='train_anno.json'))
+
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        data_prefix=dict(img='train_data_converted'),
+        ann_file='val_anno.json'))
+
+test_dataloader = val_dataloader
+
+val_evaluator = dict(ann_file=data_root + "/" + 'val_anno.json')
+
+test_evaluator = val_evaluator
+
+model = dict(bbox_head=dict(num_classes=1))
+
+
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=1.0e-5,
+        by_epoch=False,
+        begin=0,
+        end=10),
+    dict(
+        type='CosineAnnealingLR',
+        eta_min=base_lr * 0.05,
+        begin=max_epochs // 2,
+        end=max_epochs,
+        T_max=max_epochs // 2,
+        by_epoch=True,
+        convert_to_iter_based=True),
+]
+
+train_pipeline_stage2 = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='RandomResize',
+        scale=(640, 640),
+        ratio_range=(0.1, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=(640, 640)),
+    dict(type='YOLOXHSVRandomAug'),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='Pad', size=(640, 640), pad_val=dict(img=(114, 114, 114))),
+    dict(type='PackDetInputs')
+]
+
+# optimizer
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=base_lr, weight_decay=0.05),
+    paramwise_cfg=dict(
+        norm_decay_mult=0, bias_decay_mult=0, bypass_duplicate=True))
+
+default_hooks = dict(
+    checkpoint=dict(
+        interval=1,
+        max_keep_ckpts=5,
+        save_best='auto',
+        out_dir=r"..\..\..\saved_checpoints"
+    ),
+    logger=dict(type='LoggerHook', interval=5))
+
+custom_hooks = [
+    dict(
+        type='PipelineSwitchHook',
+        switch_epoch=max_epochs - stage2_num_epochs,
+        switch_pipeline=train_pipeline_stage2)
+]
+
+load_from = r"..\..\..\saved_checpoints\best_coco_bbox_mAP_epoch_7.pth"
+
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
+visualizer = dict(vis_backends=[dict(type='LocalVisBackend'),dict(type='TensorboardVisBackend')])
